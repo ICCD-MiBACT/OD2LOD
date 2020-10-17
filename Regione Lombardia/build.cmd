73297de9d8ae5955a82b05988158fdcb36b36f14
Alessandro Chiaretti <50386418+alechiaretti@users.noreply.github.com>
2020-10-17 10:28:43 +0200

    Update build.cmd

--- Regione Lombardia/build.cmd
+++ Regione Lombardia/build.cmd
@@ -1,9 +1,9 @@
 
-@call ..\bin\transform -IN intermediate\Architetture.csv2.xml -XSL xsl\1a-Architetture.xsl -OUT outArchitetture\relazioni.rdf
+@call bin\transform -IN intermediate\Architetture.csv2.xml -XSL xsl\1a-Architetture.xsl -OUT outArchitetture\relazioni.rdf
 
-@call ..\bin\transform -IN intermediate\Reperti_Archeologici_conservati_nei_Musei.csv2.xml -XSL xsl\1b-Archeologia.xsl -OUT outArcheologia\trash.txt
+@call bin\transform -IN intermediate\Reperti_Archeologici_conservati_nei_Musei.csv2.xml -XSL xsl\1b-Archeologia.xsl -OUT outArcheologia\trash.txt
 
-@call ..\bin\transform -IN intermediate\Opere_d_Arte_conservate_nei_Musei.csv2.xml -XSL xsl\1c-Opere.xsl -OUT outOpere\trash.txt
+@call bin\transform -IN intermediate\Opere_d_Arte_conservate_nei_Musei.csv2.xml -XSL xsl\1c-Opere.xsl -OUT outOpere\trash.txt
 
 @del /Q outArcheologia\trash.txt
 @del /Q outOpere\trash.txt
