4d6696a818fe3484aa0239c1a0918fb34f0ee49b
Alessandro Chiaretti <50386418+alechiaretti@users.noreply.github.com>
2020-10-18 19:04:48 +0200

    Update build.cmd

--- Regione Lombardia/build.cmd
+++ Regione Lombardia/build.cmd
@@ -1,3 +1,6 @@
+@rem csv2xml Architetture.csv -stripChar: "-splitter:; " "-splitWhat:DATAZIONE; AUTORI; USO_ATTUALE; USO_STORICO; STATO_CONSERV; STILE_SCUOLA"
+@rem csv2xml Reperti_Archeologici_conservati_nei_Musei.csv
+@rem csv2xml "Opere_d_Arte_conservate_nei_Musei.csv" "-splitter:||" "-splitWhat:CMPN||FUR||STCD||MTC||RSR||COLD||SGTT||SGTI"
 
 @call bin\transform -IN intermediate\Architetture.csv2.xml -XSL xsl\1a-Architetture.xsl -OUT outArchitetture\relazioni.rdf
 
